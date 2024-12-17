139bb792c76444adcbc5da39a1ae1f2a3841748e
Cecil-BassV <162087185+Cecil-BassV@users.noreply.github.com>
2024-12-16 20:26:49 -0700

    Throwing Knifes

---
+++ Assets/Scripts/ThrowingWeapons.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingWeapons : MonoBehaviour
+{
+    [SerializeField] Transform Camera;
+    [SerializeField] Transform attackPos;
+    [SerializeField] GameObject objectToThrow;
+
+    [SerializeField] int knifeDamage;
+    [SerializeField] int knifeDist;
+    [SerializeField] int throwMax;
+    [SerializeField] float throwCooldown;
+
+    public KeyCode throwKey = KeyCode.Q;
+    [SerializeField] float throwForce;
+    [SerializeField] float throwUpwardForce;
+
+    bool readyToThrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {      
+        readyToThrow = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Throw();
+        }
+
+        void Throw()
+        {
+            readyToThrow = false;
+
+            GameObject projectile = Instantiate(objectToThrow, attackPos.position, Camera.rotation);
+
+            Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+
+            Vector3 forceDirection = Camera.transform.forward;
+
+            RaycastHit hit;
+
+            if(Physics.Raycast(Camera.position, Camera.forward, out hit, 500f))
+            {
+                forceDirection = (hit.point - attackPos.position).normalized;
+            }
+
+            Vector3 forceAdd = forceDirection * throwForce + transform.up * throwUpwardForce;
+
+            projectileRb.AddForce(forceAdd, ForceMode.Impulse);
+
+            throwMax--;
+
+            Invoke(nameof(ResetThrow), throwCooldown);
+        }
+    }
+
+    void ResetThrow()
+    {
+        readyToThrow = true;
+    }
+}
